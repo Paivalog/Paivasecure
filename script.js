fed710b86f7a36d9542d6a88a4296a428f55a9df
Paivalog <109831201+Paivalog@users.noreply.github.com>
2024-05-28 21:53:46 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dropdown = document.querySelector('.dropdown');
-    dropdown.addEventListener('mouseover', function() {
-        dropdown.querySelector('.dropdown-content').style.display = 'block';
-    });
-    dropdown.addEventListener('mouseout', function() {
-        dropdown.querySelector('.dropdown-content').style.display = 'none';
-    });
-});
